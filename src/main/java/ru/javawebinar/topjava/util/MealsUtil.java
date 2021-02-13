0d7c0dff4c7f4ee4e6ec7f4d8f7fb0335d3fc6cf
NewTouchApp <nats2008@mail.ru>
2021-02-13 16:24:22 +0300

    add predicate to filtered

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -10,7 +10,7 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class MealsUtil {
